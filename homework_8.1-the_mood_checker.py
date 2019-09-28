3b4ac1eedcd1f4a11ff2661bcee8108242dc5747
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2019-09-28 14:48:42 +0200

    Finish Homework 8.1

---
+++ homework_8.1-the_mood_checker.py
@@ -0,0 +1,14 @@
+mood = input('How are you?: ')
+
+if mood == 'happy':
+    print("It is great to see you happy!")
+elif mood == 'nervous':
+    print("Take a deep breath 3 times.")
+elif mood == 'sad':
+    print("Studies have shown that the very act of smiling can improve your mood and make you feel happier")
+elif mood == 'excited':
+    print("Good to hear that you are excited")
+elif mood == 'relaxed':
+    print("Being relaxed is great")
+else:
+    print("I don't recognize this mood")
